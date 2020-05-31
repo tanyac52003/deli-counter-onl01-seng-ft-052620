ed513c8d97d086ffcdcb66f22e21c45b43de7a93
Tanya Hodges <tanyac52003@gmail.com>
2020-05-31 16:58:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+
+def line(katz_deli)
+  new_array = []
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else 
+    katz_deli.each_with_index {|line_member, i| 
+      puts "The line is currently: #{i + 1}. #{line_member}"}
+  end 
+end 
+
