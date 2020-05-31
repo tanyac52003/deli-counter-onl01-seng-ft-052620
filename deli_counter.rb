c6830f7f6a3d12361f24e3cb9c83ecfc7e7f992d
Tanya Hodges <tanyac52003@gmail.com>
2020-05-31 17:05:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,15 @@ def line(katz_deli)
   end 
 end 
 
+def take_a_number(katz_deli, customer)
+  katz_deli << customer
+  puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
+end 
+
+def now_serving(katz_deli)
+  if katz_deli.length > 0 
+    puts "Currently serving #{katz_deli.shift}."
+  else 
+    puts "There is nobody waiting to be served!"
+  end
+end
